f093032e2fb1ef66c570206778a32f5048a6e97b
Rhey Laurente <ra.laurente@gmail.com>
2021-01-04 16:32:11 +0800

    added new functions

--- index.js
+++ index.js
@@ -1,49 +1,69 @@
-var fs = require('fs');
-var trie = null;
+const { readFileSync } = require('fs');
+const { findWhere, filter } = require('underscore');
+const usZips = require('us-zips/map')
+
+let data_source = [];
+
 
 /**
- * Parses zip_codes.csv and creates a trie for fast lookups
+ * Parses zip_codes.csv
  */
 function loadData() {
-  var data = fs.readFileSync(__dirname + '/zip_codes.csv', 'utf8');
-  var lines = data.split('\r\n');
-  var trie = {};
+  let data = readFileSync(__dirname + '/zip_codes.csv', 'utf8');
+  let lines = data.split('\r\n');
 
-  lines.forEach(function(line) {
-    var parts = line.split(',');
-    var zip = parts[0], city = parts[1], state = parts[2];
-    var node = trie;
-  
-    for (var i = 0; i < zip.length; i++) {
-      var num = zip[i];
-      var pos = node[num];
-      if (pos == null)
-        node = node[num] = (i === zip.length - 1) ? [city, state] : {};
-      else
-        node = node[num];
+  for(let item of lines){
+    let parts = item.split(',');
+    let _item = {
+      zip_code: parts[0],
+      city: parts[1],
+      state: parts[2]
+    };
+
+    let location = usZips.get(_item.zip_code);
+
+    if(location){
+      _item = {
+        ..._item,
+        ...location
+      }
     }
-  });
-  
-  return trie;
+
+    data_source.push(_item);
+  }
 }
 
 /**
  * Lookup a zipcode
- * Returns data in [city, state] format
+ * Returns data in {zip_code, city, state } format
  */
-exports.lookup = function(zip) {
-  if (zip.length < 5)
-    return null;
-    
-  if (!trie)
-    trie = loadData();
-    
-  var node = trie;
-  for (var i = 0; i < zip.length; i++) {
-    var node = node[zip[i]];
-    if (node == null)
-      return null;
+exports.lookup = function(zipcode) {
+  if(data_source.length === 0){
+    loadData();
   }
-  
-  return Array.isArray(node) ? node : null;
+
+  let result = findWhere(data_source, {
+    zip_code: zipcode
+  });
+  if(result){
+    return result;
+  }
 };
+
+/**
+ * listing by state
+ * Returns data in {zip_code, city, state } format
+ */
+exports.listByState = function(state){
+  if(data_source.length === 0){
+    loadData();
+  }
+
+  let result = filter(data_source, item=>{
+    return state.toUpperCase() === item.state;
+  });
+
+  if(result){
+    return result;
+  }
+}
